d674a0209344166524132ca7a221f15a3610ebc5
Scott Bromander <scott@primeacademy.io>
2016-07-09 21:03:52 -0500

    adds moving background

---
+++ Assets/Scripts/RepeatingBackground.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour {
+	[Tooltip("How fast the speed should be")]
+	public float scrollSpeed;
+
+	/// <summary>
+	/// How far to move to until the image is offscreen
+	/// </summary>
+	public const float ScrollWidth = 8; //bg width / pixels per unit;
+
+	/// <summary>
+	/// Called at a fixed time rate, moves the objects and if they are off screen to the things.
+	/// </summary>
+	private void FixedUpdate() {
+		Vector3 pos = transform.position;
+
+		pos.x -= scrollSpeed * Time.deltaTime;
+
+		if(transform.position.x < -ScrollWidth){
+			Offscreen (ref pos);
+		}
+
+		transform.position = pos;
+	}
+
+	/// <summary>
+	/// Called whenever the object this is attached to goes completely off-screen
+	/// </summary>
+	protected virtual void Offscreen(ref Vector3 pos){
+		pos.x += 2 * ScrollWidth;
+	}
+}
