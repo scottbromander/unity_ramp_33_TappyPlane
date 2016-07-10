7ff4cd6b30f427a8e6742c7f77835f732c540385
Scott Bromander <scott@primeacademy.io>
2016-07-09 21:32:16 -0500

    adds player controller

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerBehaviour : MonoBehaviour {
+
+	[Tooltip("The force which is added when the player jumps")]
+	public Vector2 jumpForce = new Vector2 (0, 300);
+
+	/// <summary>
+	/// If we have been hit, we can no longer jump
+	/// </summary>
+	private bool beenHit;
+
+	private Rigidbody2D rigidBody2D;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody2D = GetComponent<Rigidbody2D> ();
+	}
+
+	void LateUpdate(){
+		if ((Input.GetKeyUp (KeyCode.Space) || Input.GetMouseButton (0)) && !beenHit) {
+			rigidBody2D.velocity = Vector2.zero;
+			GetComponent<Rigidbody2D>().AddForce (jumpForce);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		beenHit = true;
+
+		GetComponent<Animator> ().speed = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
