8a1a922762929023c5efb1f229490c54afaa697b
Scott Bromander <scott@primeacademy.io>
2016-07-09 23:43:07 -0500

    adds creation of obstacles

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -9,13 +9,29 @@ public class GameController : MonoBehaviour {
 	/// </summary>
 	public static float speedModifier;
 
+	[Header("Obstacle Information")]
+
+	[Tooltip("The obstacle that we will spawn")]
+	public GameObject obstacleReference;
+
+	[Tooltip("Min Y value for the obstacle")]
+	public float obstacleMinY = -1.3f;
+
+	[Tooltip("Max Y value for the obstacle")]
+	public float obstacleMaxY = 1.3f;
+
 	// Use this for initialization
 	void Start () {
 		speedModifier = 1.0f;
+		InvokeRepeating ("CreateObstacle", 1.5f, 1.0f);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	/// <summary>
+	/// Creates an Obstacle an initializes its position
+	/// </summary>
+	void CreateObstacle() {
+		Instantiate (obstacleReference, new Vector3 (RepeatingBackground.ScrollWidth, 
+			Random.Range (obstacleMinY, obstacleMaxY), 0.0f), Quaternion.identity);
 	}
+
 }
